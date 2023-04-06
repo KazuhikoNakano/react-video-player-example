96560e9997624a9f5979dd381ea46907e1f8ce71
KazuhikoNakano <kazuhiko.nakano@adglobe.co.jp>
2023-04-06 16:33:47 +0900

    一定秒数後に停止するように変更

--- src/pages/video.tsx
+++ src/pages/video.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import dynamic from 'next/dynamic';
 
 const ReactPlayer = dynamic(() => import('react-player'), {
   ssr: false,
 });
 
-const VideosPage = () => {
+const VideosPage: React.FC = () => {
+  const [isPlaying, setIsPlaying] = useState(true);
+
+  const handleProgress = (progress: { playedSeconds: number }) => {
+    console.log(progress.playedSeconds);
+    if (progress.playedSeconds >= 3) {
+      console.log('stop');
+      setIsPlaying(false);
+    }
+  };
+
   return (
     <main className='flex justify-center'>
       <div>
@@ -13,11 +23,12 @@ const VideosPage = () => {
         <div className='[&_iframe]:rounded-md'>
           <ReactPlayer
             url="https://youtube.com/shorts/ShadyfN8w1U?feature=share" // 動画のURLは変更してください
-            playing
+            playing={isPlaying}
             controls
             muted
             width={159}
             height={283}
+            onProgress={handleProgress}
           />
         </div>
       </div>
