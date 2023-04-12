9b82554c08ce3f8a1b60b496f7c85b8165e0535a
KazuhikoNakano <kazuhiko.nakano@adglobe.co.jp>
2023-04-12 18:06:29 +0900

    連続再生とドラッグでの再生に対応

--- src/pages/video.tsx
+++ src/pages/video.tsx
@@ -9,86 +9,71 @@ const ReactPlayer = dynamic(() => import('react-player'), {
 
 
 const VideosPage: React.FC = () => {
-  const [isPlaying, setIsPlaying] = useState([true, false, false, false]);
-  const [isMuted, setIsMuted] = useState([true, true, true, true]);
-  const [sliderRef, instanceRef] = useKeenSlider({
+  const [isPlaying, setIsPlaying] = useState([true, false, false]);
+  const [isMuted, setIsMuted] = useState([true, true, true]);
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
     vertical: true,
     loop: true,
-  });
+  },
+  [
+    (slider) => {
+      let timeout: ReturnType<typeof setTimeout>
+      const clearNextTimeout = () => {
+        clearTimeout(timeout)
+      }
+      const nextTimeout = () => {
+        clearTimeout(timeout)
+        timeout = setTimeout(() => {
+          slider.next()
+        }, 10000)
+      }
+      const changedSlide = (index: number) => {
+        setIsPlaying(prevState => {
+          const newState = [false, false, false];
+          newState[index] = true;
+          return newState;
+        });
+      }
+
+      slider.on("created", () => {
+        nextTimeout()
+      })
+      slider.on("dragStarted", clearNextTimeout)
+      slider.on("animationEnded", nextTimeout)
+      slider.on("updated", nextTimeout)
+      slider.on("slideChanged", (s) => {
+        changedSlide(s.track.details.rel);
+      })
+    },
+  ]);
 
   const videoUrls = [
     "https://www.youtube.com/shorts/ZnlCbv9-d9M",
     "https://www.youtube.com/shorts/ZnlCbv9-d9M",
-    "https://vimeo.com/816127333",
-    "https://vimeo.com/816127333"
+    "https://www.youtube.com/shorts/ZnlCbv9-d9M",
+    // "https://vimeo.com/816127333",
+    // "https://vimeo.com/816127333",
+    // "https://vimeo.com/816127333",
   ];
 
-  const handleProgress = (index: number) => (progress: { playedSeconds: number }) => {
-    console.log(progress.playedSeconds);
-    // 一定秒数後に停止
-    // if (progress.playedSeconds >= 10) {
-    //   console.log('stop');
-    //   setIsPlaying(prevState => {
-    //     const newState = [...prevState];
-    //     newState[index] = false;
-    //     return newState;
-    //   });
-    // }
-  };
-
-  const resolveNextIndex = (index: number) => {
-    if (index === videoUrls.length - 1) {
-      return 0;
-    }
-    return index + 1;
-  };
-
-  const handlePlayVideo = (index: number) => {
-    console.log('play');
-    console.log(index);
-    setIsPlaying(prevState => {
-      const newState = [...prevState];
-      newState[index] = true;
-      return newState;
-    });
-    setIsMuted(prevState => {
-      const newState = [...prevState];
-      newState[index] = false;
-      return newState;
-    });
-  };
-
-  const handleNextSlide = (index: number): void => {
-    console.log('next');
-    setIsPlaying(prevState => {
-      const newState = [...prevState];
-      newState[index] = false;
-      newState[resolveNextIndex(index)] = true;
-      return newState;
-    });
-    // setIsMuted(prevState => {
-    //   const newState = [...prevState];
-    //   newState[resolveNextIndex(index)] = false;
-    //   return newState;
-    // });
+  const handleNextSlide = (): void => {
     instanceRef.current && instanceRef.current.next();
   };
 
   return (
     <main className='flex justify-center'>
       <div>
-        <div ref={sliderRef} className="keen-slider w-screen h-[100dvh]">
+        <div ref={sliderRef} className="keen-slider w-full h-[100dvh]">
         {videoUrls.map((url, index) => (
           <div key={index} className="keen-slider__slide relative">
-            <div className='w-full h-[95dvh]'>
+            <div className='w-[100%] min-w-[300px] h-[95dvh]'>
               <ReactPlayer
                 url={url}
                 playing={isPlaying[index]}
-                muted={isMuted[index]}
-                controls={true}
+                muted
+                controls
                 width={'100%'}
                 height={'100%'}
-                onProgress={handleProgress(index)}
                 config={{
                   youtube: {
                     playerVars: {
@@ -105,16 +90,15 @@ const VideosPage: React.FC = () => {
                       byline: false,
                       portrait: false,
                       title: false,
-                      transparent: 0,
+                      transparent: true,
                     },
                   },
                 }}
               />
             </div>
-            {/* 下部操作部分 */}
             <div
               className="bg-black w-full h-10 flex items-center justify-center absolute bottom-0 cursor-pointer z-10"
-              onClick={() => {handleNextSlide(index)}}
+              onClick={() => {handleNextSlide()}}
             >
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="text-white w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
